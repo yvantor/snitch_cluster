gioelegott/snitch

snitch_cluster: CSR-based HW barrier

hw/ip/snitch/src/riscv_instr.sv
@@ -1101,5 +1101,6 @@ package riscv_instr;
   localparam logic [11:0] CSR_MHPMCOUNTER29H = 12'hb9d;
   localparam logic [11:0] CSR_MHPMCOUNTER30H = 12'hb9e;
   localparam logic [11:0] CSR_MHPMCOUNTER31H = 12'hb9f;
+  localparam logic [11:0] CSR_BARRIER = 12'hba0;
 endpackage
 // verilog_lint: waive-stop parameter-name-style

sw/snRuntime/src/sync.h
@@ -63,12 +63,7 @@ inline void snrt_mutex_release(volatile uint32_t *pmtx) {
 
 /// Synchronize cores in a cluster with a hardware barrier
 inline void snrt_cluster_hw_barrier() {
-    uint32_t register r;
-
-    asm volatile("lw %0, 0(%1)"
-                 : "=r"(r)
-                 : "r"((uint32_t)snrt_cluster_hw_barrier_addr())
-                 : "memory");
+    asm volatile("csrr x0, 0xBA0" ::: "memory");
 }
 
 inline void snrt_reset_barrier() { _snrt_barrier = 0; }

yvantor/snitch_cluster

hw/snitch/src/riscv_instr.sv
@@ -1133,6 +1133,7 @@ package riscv_instr;
   localparam logic [11:0] CSR_MHARTID = 12'hf14;
   localparam logic [11:0] CSR_SSR = 12'h7c0;
   localparam logic [11:0] CSR_FPMODE = 12'h7c1;
+  localparam logic [11:0] CSR_BARRIER = 12'h7c2;
   localparam logic [11:0] CSR_HTIMEDELTAH = 12'h615;
   localparam logic [11:0] CSR_CYCLEH = 12'hc80;
   localparam logic [11:0] CSR_TIMEH = 12'hc81;

sw/snRuntime/src/sync.h
@@ -56,12 +56,7 @@ inline void snrt_mutex_release(volatile uint32_t *pmtx) {
 
 /// Synchronize cores in a cluster with a hardware barrier
 inline void snrt_cluster_hw_barrier() {
-    uint32_t register r;
-
-    asm volatile("lw %0, 0(%1)"
-                 : "=r"(r)
-                 : "r"((uint32_t)snrt_cluster_hw_barrier_addr())
-                 : "memory");
+    asm volatile("csrr x0, 0x7C2" ::: "memory");
 }
 
 /// Synchronize clusters globally with a global software barrier