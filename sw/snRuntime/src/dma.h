214b6f6c4f10c88a54a6dc78bf0031a2374272c0
Tim Fischer <fischeti@iis.ee.ethz.ch>
2024-05-28 08:49:36 +0200

    sw: Clean up runtime and add support for multi channel DMA

--- sw/snRuntime/src/dma.h
+++ sw/snRuntime/src/dma.h
@@ -10,49 +10,11 @@ typedef uint32_t snrt_dma_txid_t;
 /// Initiate an asynchronous 1D DMA transfer with wide 64-bit pointers.
 inline snrt_dma_txid_t snrt_dma_start_1d_wideptr(uint64_t dst, uint64_t src,
                                                  size_t size) {
-    // Current DMA does not allow transfers with size == 0 (blocks)
-    // TODO(colluca) remove this check once new DMA is integrated
-    if (size > 0) {
-        register uint32_t reg_dst_low asm("a0") = dst >> 0;    // 10
-        register uint32_t reg_dst_high asm("a1") = dst >> 32;  // 11
-        register uint32_t reg_src_low asm("a2") = src >> 0;    // 12
-        register uint32_t reg_src_high asm("a3") = src >> 32;  // 13
-        register uint32_t reg_size asm("a4") = size;           // 14
-
-        // dmsrc a2, a3
-        asm volatile(
-            ".word (0b0000000 << 25) | \
-                (     (13) << 20) | \
-                (     (12) << 15) | \
-                (    0b000 << 12) | \
-                (0b0101011 <<  0)   \n" ::"r"(reg_src_high),
-            "r"(reg_src_low));
-
-        // dmdst a0, a1
-        asm volatile(
-            ".word (0b0000001 << 25) | \
-                (     (11) << 20) | \
-                (     (10) << 15) | \
-                (    0b000 << 12) | \
-                (0b0101011 <<  0)   \n" ::"r"(reg_dst_high),
-            "r"(reg_dst_low));
-
-        // dmcpyi a0, a4, 0b00
-        register uint32_t reg_txid asm("a0");  // 10
-        asm volatile(
-            ".word (0b0000010 << 25) | \
-                (  0b00000 << 20) | \
-                (     (14) << 15) | \
-                (    0b000 << 12) | \
-                (     (10) <<  7) | \
-                (0b0101011 <<  0)   \n"
-            : "=r"(reg_txid)
-            : "r"(reg_size));
-
-        return reg_txid;
-    } else {
-        return -1;
-    }
+    register uint32_t reg_txid;  // 10
+    asm volatile("dmsrc   %[sl], %[sh]"     :: [sh]"r"(src >> 32), [sl]"r"(src));
+    asm volatile("dmdst   %[dl], %[dh]"     :: [dh]"r"(dst >> 32), [dl]"r"(dst));
+    asm volatile("dmcpyi  %[id], %[sz], 0"  : [id]"=r"(reg_txid) : [sz]"r"(size));
+    return reg_txid;
 }
 
 /// Initiate an asynchronous 1D DMA transfer.
@@ -66,71 +28,13 @@ inline snrt_dma_txid_t snrt_dma_start_2d_wideptr(uint64_t dst, uint64_t src,
                                                  size_t size, size_t dst_stride,
                                                  size_t src_stride,
                                                  size_t repeat) {
-    // Current DMA does not allow transfers with size == 0 (blocks)
-    // TODO(colluca) remove this check once new DMA is integrated
-    if (size > 0) {
-        register uint32_t reg_dst_low asm("a0") = dst >> 0;       // 10
-        register uint32_t reg_dst_high asm("a1") = dst >> 32;     // 11
-        register uint32_t reg_src_low asm("a2") = src >> 0;       // 12
-        register uint32_t reg_src_high asm("a3") = src >> 32;     // 13
-        register uint32_t reg_size asm("a4") = size;              // 14
-        register uint32_t reg_dst_stride asm("a5") = dst_stride;  // 15
-        register uint32_t reg_src_stride asm("a6") = src_stride;  // 16
-        register uint32_t reg_repeat asm("a7") = repeat;          // 17
-
-        // dmsrc a0, a1
-        asm volatile(
-            ".word (0b0000000 << 25) | \
-                (     (13) << 20) | \
-                (     (12) << 15) | \
-                (    0b000 << 12) | \
-                (0b0101011 <<  0)   \n" ::"r"(reg_src_high),
-            "r"(reg_src_low));
-
-        // dmdst a0, a1
-        asm volatile(
-            ".word (0b0000001 << 25) | \
-                (     (11) << 20) | \
-                (     (10) << 15) | \
-                (    0b000 << 12) | \
-                (0b0101011 <<  0)   \n" ::"r"(reg_dst_high),
-            "r"(reg_dst_low));
-
-        // dmstr a5, a6
-        asm volatile(
-            ".word (0b0000110 << 25) | \
-                (     (15) << 20) | \
-                (     (16) << 15) | \
-                (    0b000 << 12) | \
-                (0b0101011 <<  0)   \n"
-            :
-            : "r"(reg_dst_stride), "r"(reg_src_stride));
-
-        // dmrep a7
-        asm volatile(
-            ".word (0b0000111 << 25) | \
-                (     (17) << 15) | \
-                (    0b000 << 12) | \
-                (0b0101011 <<  0)   \n"
-            :
-            : "r"(reg_repeat));
-
-        // dmcpyi a0, a4, 0b10
-        register uint32_t reg_txid asm("a0");  // 10
-        asm volatile(
-            ".word (0b0000010 << 25) | \
-                (  0b00010 << 20) | \
-                (     (14) << 15) | \
-                (    0b000 << 12) | \
-                (     (10) <<  7) | \
-                (0b0101011 <<  0)   \n"
-            : "=r"(reg_txid)
-            : "r"(reg_size));
-
-        return reg_txid;
-    } else {
-        return -1;
-    }
+    register uint32_t reg_txid;  // 10
+    asm volatile("dmsrc   %[sl], %[sh]"     :: [sh]"r"(src >> 32), [sl]"r"(src));
+    asm volatile("dmdst   %[dl], %[dh]"     :: [dh]"r"(dst >> 32), [dl]"r"(dst));
+    asm volatile("dmstr   %[rd], %[rs]"     :: [rd]"r"(dst_stride), [rs]"r"(src_stride));
+    asm volatile("dmrep   %[rp]"            :: [rp]"r"(repeat));
+    asm volatile("dmcpyi  %[id], %[sz], 2"  : [id]"=r"(reg_txid) : [sz]"r"(size));
+    return reg_txid;
 }
 
 /// Initiate an asynchronous 2D DMA transfer.
@@ -141,33 +45,61 @@ inline snrt_dma_txid_t snrt_dma_start_2d(void *dst, const void *src,
                                      src_stride, repeat);
 }
 
+/// Initiate an asynchronous 1D DMA transfer with wide 64-bit pointers and a specific channel.
+inline snrt_dma_txid_t snrt_dma_start_1d_channel_wideptr(uint64_t dst, uint64_t src,
+                                                         size_t size, uint32_t channel) {
+        register uint32_t reg_txid;  // 10
+        register uint32_t cfg = channel << 2;
+        asm volatile("dmsrc   %[sl], %[sh]"         :: [sh]"r"(src >> 32), [sl]"r"(src));
+        asm volatile("dmdst   %[dl], %[dh]"         :: [dh]"r"(dst >> 32), [dl]"r"(dst));
+        asm volatile("dmcpy  %[id], %[sz], %[cfg]"  : [id]"=r"(reg_txid) : [sz]"r"(size), [cfg]"r"(cfg));
+        return reg_txid;
+}
+
+/// Initiate an asynchronous 1D DMA transfer and a specific channel.
+inline snrt_dma_txid_t snrt_dma_start_1d_channel(void *dst, const void *src,
+                                         size_t size, uint32_t channel) {
+    return snrt_dma_start_1d_channel_wideptr((size_t)dst, (size_t)src, size, channel);
+}
+
+/// Initiate an asynchronous 1D DMA transfer with wide 64-bit pointers and a specific channel.
+inline snrt_dma_txid_t snrt_dma_start_2d_channel_wideptr(uint64_t dst, uint64_t src,
+                                                         size_t size, uint32_t channel) {
+        register uint32_t reg_txid;  // 10
+        register uint32_t cfg = channel << 2 | 2;
+        asm volatile("dmsrc   %[sl], %[sh]"         :: [sh]"r"(src >> 32), [sl]"r"(src));
+        asm volatile("dmdst   %[dl], %[dh]"         :: [dh]"r"(dst >> 32), [dl]"r"(dst));
+        asm volatile("dmcpy  %[id], %[sz], %[cfg]"  : [id]"=r"(reg_txid) : [sz]"r"(size), [cfg]"r"(cfg));
+        return reg_txid;
+}
+
+/// Initiate an asynchronous 2D DMA transfer and a specific channel.
+inline snrt_dma_txid_t snrt_dma_start_2d_channel(void *dst, const void *src,
+                                         size_t size, uint32_t channel) {
+    return snrt_dma_start_2d_channel_wideptr((size_t)dst, (size_t)src, size, channel);
+}
+
 /// Block until a transfer finishes.
 inline void snrt_dma_wait(snrt_dma_txid_t tid) {
+    register uint32_t tmp;
     // dmstati t0, 0  # 2=status.completed_id
     asm volatile(
         "1: \n"
-        ".word (0b0000100 << 25) | \
-               (  0b00000 << 20) | \
-               (    0b000 << 12) | \
-               (      (5) <<  7) | \
-               (0b0101011 <<  0)   \n"
+        "dmstati  %[tmp], 0 \n"
         "sub t0, t0, %0 \n"
-        "blez t0, 1b \n" ::"r"(tid)
-        : "t0");
+        "blez t0, 1b \n"
+        : [tmp]"=&r"(tmp) :"r"(tid) : "t0");
 }
 
 /// Block until all operation on the DMA ceases.
 inline void snrt_dma_wait_all() {
+    register uint32_t tmp;
     // dmstati t0, 2  # 2=status.busy
     asm volatile(
         "1: \n"
-        ".word (0b0000100 << 25) | \
-               (  0b00010 << 20) | \
-               (    0b000 << 12) | \
-               (      (5) <<  7) | \
-               (0b0101011 <<  0)   \n"
-        "bne t0, zero, 1b \n" ::
-            : "t0");
+        "dmstati  %[tmp], 2 \n"
+        "bne %[tmp], zero, 1b \n"
+        : [tmp]"=&r"(tmp) :: "t0");
 }
 
 /**
